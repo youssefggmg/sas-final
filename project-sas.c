0203c308abe968dbf6d7055ca0aa272f4ddd412e
youssef elhoubi <121304134+youssefggmg@users.noreply.github.com>
2024-09-11 00:10:25 +0100

    complaited a add function

--- project-sas.c
+++ project-sas.c
@@ -25,52 +25,157 @@ typedef struct sas
     int point;
 } student;
 
-void add_student(student arr[], int index)
+void add_student(student arr[], int index, char departments[])
 {
+    int choice, n;
     system(CLEAR);
-    printf("what is the student first name:");
-    while (scanf(" %[^\n]s", arr[index].FirstName) != 1)
+    printf("do you want to add student or mutiple studens\n 1.single student \n 2.multiple students");
+
+    while (scanf("%d", &choice) != 1)
     {
-        getchar();
-        printf("invalid first name\n");
-        scanf(" %[^\n]s", arr[index].FirstName);
+        printf("Invalid input. Please enter a number.\n");
+        system(CLEAR);
+        scanf("%d", choice);
     }
-    getchar();
-    sleep(1);
-    printf("what is the student last name:");
-    while (scanf(" %[^\n]s", arr[index].LastName) != 1)
+    switch (choice)
     {
+        // add only one student
+    case 1:
+        printf("what is the student first name:");
+        while (scanf(" %[^\n]s", arr[index].FirstName) != 1)
+        {
+            getchar();
+            printf("invalid first name\n");
+            scanf(" %[^\n]s", arr[index].FirstName);
+        }
         getchar();
-        printf("invalid last name \n");
-        printf("reenter the last name \n");
-        scanf(" %[^\n]s", arr[index].LastName);
-    }
-    getchar();
-    sleep(1);
-    printf("what is the student date of birth:");
-    while (scanf(" %[^\n]s", arr[index].dathOfBearth) != 1)
-    {
+        sleep(1);
+        printf("what is the student last name:");
+        while (scanf(" %[^\n]s", arr[index].LastName) != 1)
+        {
+            getchar();
+            printf("invalid last name \n");
+            printf("reenter the last name \n");
+            scanf(" %[^\n]s", arr[index].LastName);
+        }
         getchar();
-        printf("invalid input\n");
-        scanf(" %[^\n]s", arr[index].dathOfBearth);
-    }
-    getchar();
-    sleep(1);
-    printf("printf  what is the student department:\n");
-    printf("1. Computer Science\n");
-    printf("2. Mathematics\n");
-    printf("3. Physics\n");
-    printf("4. Chemistry\n");
-    printf("5. Biology\n");
-    printf("6. Economics\n");
-    printf("7. Psychology\n");
-    printf("8. Engineering\n");
-    while (scanf("%d", arr[index].department) != 1)
-    {
+        sleep(1);
+        printf("what is the student date of birth:");
+        while (scanf(" %[^\n]s", arr[index].dathOfBearth) != 1)
+        {
+            getchar();
+            printf("invalid input\n");
+            scanf(" %[^\n]s", arr[index].dathOfBearth);
+        }
         getchar();
-        printf("invalid department\n");
-        printf("reenter the department \n");
-        scanf("%d", arr[index].department);
+        sleep(1);
+        printf("printf  what is the student department:\n");
+        printf("1. Computer Science\n");
+        printf("2. Mathematics\n");
+        printf("3. Physics\n");
+        printf("4. Chemistry\n");
+        printf("5. Biology\n");
+        printf("6. Economics\n");
+        printf("7. Psychology\n");
+        printf("8. Engineering\n");
+        while (scanf("%d", &choice) != 1 || choice <= 1 || choice >= 8)
+        {
+            printf("invalid choice\n");
+            printf("reenter the choice\n");
+            scanf("%d", &choice);
+        }
+        strcpy(arr[index].department, departments[choice]);
+        printf("printf  what is the student point:\n");
+        while (scanf("%d", arr[index].point) != 1)
+        {
+            printf("invalid input\n");
+            scanf("%d", &arr[index].point);
+        }
+        ++index;
+        printf("you have added a student to the list do you want to contenu:1.contenu \n 2.stop ");
+        scanf("%s", &choice);
+        if (choice != 1)
+        {
+            exit(0);
+        }
+        break;
+        // add more than one student
+    case 2:
+        int counter = 0;
+        printf("how may do you want to add");
+
+        while (scanf("%d", &n) != 1)
+        {
+            printf("invalid input\n");
+            scanf("%d", &n);
+        }
+        for (int i = index; i < index + n; i++)
+        {
+            system(CLEAR);
+            printf("what is the student first name: number %d",counter);
+            while (scanf(" %[^\n]s", arr[i].FirstName) != 1)
+            {
+                getchar();
+                printf("invalid first name\n");
+                scanf(" %[^\n]s", arr[i].FirstName);
+            }
+            getchar();
+            sleep(1);
+            printf("what is the student last name: number %d",counter);
+
+            while (scanf(" %[^\n]s", arr[i].LastName) != 1)
+            {
+                getchar();
+                printf("invalid last name \n");
+                printf("reenter the last name \n");
+                scanf(" %[^\n]s", arr[i].LastName);
+            }
+            getchar();
+            sleep(1);
+            printf("what is the student date of birth: number %d",counter);
+
+            while (scanf(" %[^\n]s", arr[i].dathOfBearth) != 1)
+            {
+                getchar();
+                printf("invalid input\n");
+                scanf(" %[^\n]s", arr[i].dathOfBearth);
+            }
+            getchar();
+            sleep(1);
+            printf("printf  what is the student department:\n");
+            printf("1. Computer Science\n");
+            printf("2. Mathematics\n");
+            printf("3. Physics\n");
+            printf("4. Chemistry\n");
+            printf("5. Biology\n");
+            printf("6. Economics\n");
+            printf("7. Psychology\n");
+            printf("8. Engineering\n");
+            while (scanf("%d", &choice) != 1 || choice <= 1 || choice >= 8)
+            {
+                printf("invalid choice\n");
+                printf("reenter the choice\n");
+                scanf("%d", &choice);
+            }
+            strcpy(arr[i].department, departments[choice]);
+            printf("printf  what is the student point:number %d,counter \n");
+
+            while (scanf("%d", arr[i].point) != 1)
+            {
+                printf("invalid input\n");
+                scanf("%d", &arr[i].point);
+            }
+            ++counter;
+        }
+        printf("all of the students wher added\n");
+        printf("do you whant to continu\n");
+        printf("all the sudents to the list do you want to contenu:1.contenu \n 2.stop ");
+        scanf("%s", &choice);
+        if (choice != 1)
+        {
+            exit(0);
+        }
+        break;
     }
 }
 
@@ -108,7 +213,7 @@ int main()
         switch (choice)
         {
         case 1:
-            add_student(students, index);
+            add_student(students, index, departments);
             break;
         }
     }
